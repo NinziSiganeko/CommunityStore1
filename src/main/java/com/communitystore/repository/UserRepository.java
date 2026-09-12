9af356c43e99b1206eeab1034e7a7c52bd176818
NinziSiganeko <ninzisiganeko@gmail.com>
2026-09-12 06:03:09 +0200

    update

--- src/main/java/com/communitystore/repository/UserRepository.java
+++ src/main/java/com/communitystore/repository/UserRepository.java
@@ -1,9 +1,24 @@
 package com.communitystore.repository;
+
 import com.communitystore.domain.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
     Optional<User> findByEmailIgnoreCase(String email);
+
+
     boolean existsByEmailIgnoreCase(String email);
+
+
     boolean existsByUsernameIgnoreCase(String username);
+
+
+    Optional<User> findByUsernameIgnoreCase(String username);
 }
